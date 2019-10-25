ad0445ae87ae995af6334d3ff40a8d4d3a2bcf00
Jo Roe <jroe@invoca.com>
2019-10-25 13:53:10 -0700

    found actual bug with unrelased pokemon

--- spec/pokedex_scraper_spec.rb
+++ spec/pokedex_scraper_spec.rb
@@ -4,8 +4,14 @@ describe PokedexScraper do
 
   scraper = PokedexScraper.new
 
-  it "finds and displays a pokemon" do
+  it "finds and displays a single pokemon" do
     pokemon = scraper.find_and_display_pokemon('squirtle')
+    expect(pokemon).to be_a(Array)
+    expect(pokemon.length).to be 1
+  end
+
+  it "finds and displays a single pokemon with image" do
+    pokemon = scraper.find_and_display_pokemon('squirtle', true)
     expect(pokemon).to be_a(Array)
     expect(pokemon.length).to be 1
   end
@@ -15,5 +21,23 @@ describe PokedexScraper do
     pokemon = scraper.find_and_display_pokemon(fake_pokemon)
     expect(pokemon).to be_a(Array)
     expect(pokemon[0]).to eq("No pokemon found")
+  end
+
+  it "doesn't find a pokemon with image" do
+    pokemon = scraper.find_and_display_pokemon('baloney', true)
+    expect(pokemon).to be_a(Array)
+    expect(pokemon[0]).to eq("No pokemon found")
+  end
+
+  it "finds a single unreleased pokemon" do
+    pokemon = scraper.find_and_display_pokemon('audino')
+    expect(pokemon).to be_a(Array)
+    expect(pokemon.length).to be 1
+  end
+
+  it "finds a single unreleased pokemon with image" do
+    pokemon = scraper.find_and_display_pokemon('audino', true)
+    expect(pokemon).to be_a(Array)
+    expect(pokemon.length).to be 1
   end
 end
